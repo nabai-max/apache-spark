c7a0d2e67e6ec5f4774e0314a986c7efc30b1d6e
Kiet T. Tran, Ph.D <ktran@harrisburu.edu>
2019-11-11 06:21:52 -0500

    rearranged files'

--- performance.py
+++ performance.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+
 from pyspark import SparkContext, SparkConf, SQLContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import desc
+import subprocess
 
 import sys
 
@@ -11,22 +15,22 @@ class Performance():
     def load_year(self, year):        
         spark = SparkSession.builder.appName("performance-app").config("spark.config.option", "value").getOrCreate()
         self.df = spark.read.option("header", "true").csv(year)
-        # self.df = spark.read.option("header", "true").csv(year).select('*').toPandas()
         return self.df
     
-    
+    def originated_airport_with_most_flights(self, out_dir):
+        orig_airports = self.df.groupBy('Origin').count().orderBy(desc('count'))
+        self.sc.parallelize(orig_airports.collect()).saveAsTextFile(out_dir)
+        return orig_airports.first()
+        
+def delete_out_dir(out_dir):
+        subprocess.call(["hdfs", "dfs", "-rm", "-R", out_dir])           
+        
 def main(argv):
+    delete_out_dir(argv[1])
     perf = Performance()
-    df = perf.load_year(argv[0])
-    df[0]
-    print(df)
-    
-    # for row in df.itertuples():
-    #     buf = ''
-    #     for col in range(1, len(row)):
-    #         buf += row[col] + ' '
-    #     print(buf)
-
+    perf.load_year(argv[0])
+    most_count = perf.originated_airport_with_most_flights(argv[1])
+    print('{} has the most originated flights at {}'.format(most_count['Origin'], most_count['count']))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
